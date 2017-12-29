4cbec2a83972c4c27a1580cbdc528648eb587917
tunadao1 <hugorafaelmc@gmail.com>
2017-12-29 19:58:43 -0200

    Initial PWA

--- public/src/js/app/app.js
+++ public/src/js/app/app.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker
+    .register('../../service-worker.js')
+    .then(function() { console.log('Service Worker Registered'); });
+}
+
 import { AppController } from './controller/AppController';
 
 let appController = new AppController();
